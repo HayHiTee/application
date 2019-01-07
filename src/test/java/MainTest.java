f313eac56aeb0a816c02199ee17c4dcdbdfb8e59
hayhitee <temitope.akintilebo@crop2cash.com.ng>
2019-01-07 10:43:51 +0100

    Java Application Test Solution and Jar Application

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void readInput() {
+        ArrayList<Person> people = Main.readInput("");
+        assertNotNull(people);
+    }
+
+    @Test
+    public void printCities() {
+    }
+
+    @Test
+    public void printPeopleToCity() {
+    }
+
+    @Test
+    public void main() {
+    }
+}
---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PersonTest {
+
+    @Test
+    public void compareId() {
+    }
+
+    @Test
+    public void equals() {
+    }
+
+    @Test
+    public void getCity() {
+    }
+
+    @Test
+    public void compareCity() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void getNameAndId() {
+    }
+}
